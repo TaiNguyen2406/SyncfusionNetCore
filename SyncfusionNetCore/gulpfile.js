d8a2d8a04949bbcf89b6a529187d97b37dbb4071
nguyễn duy tài <underwaterhp93@gmail.com>
2019-10-15 00:11:49 +0700

    Add project files.

---
+++ SyncfusionNetCore/gulpfile.js
@@ -0,0 +1,23 @@
+﻿// command to automatically run the gulp task each time building the web application
+
+/// <binding BeforeBuild='copy-client-resource'/>
+
+// nodejs requiring statement for importing and using the package in this js file
+var gulp = require('gulp');
+var glob = require('glob');
+
+
+// gulp task for copying file form “node_modules” to “wwwroot” directory
+gulp.task("copy-client-resource", function (done) {
+    let packagePath = './node_modules/@syncfusion/';
+    let destCommonPath = 'wwwroot/syncfusion';
+    let installedPackages = glob.sync(`${packagePath}*`);
+    for (let insPackage of installedPackages) {
+        let packagename = insPackage.replace(packagePath, '');
+        gulp.src(`${insPackage}/dist/global/**/*`)
+            .pipe(gulp.dest(`${destCommonPath}/${packagename}/`));
+        gulp.src(`${insPackage}/styles/**/*.css`)
+            .pipe(gulp.dest(`${destCommonPath}/${packagename}/styles/`));
+    }
+    done();
+});
